af5030bfc2027483bcf66d50277175731935243a
PROMETHEUS <prometheus@agent.local>
2026-06-25 21:30:02 +0000

    PROMETHEUS: Auto-Sync 2026-06-25 21:30:02 UTC

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
-from app import create_app
-app = create_app()
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=11435)
+from flask import Flask
+app = Flask(__name__)
+@app.route('/hello')
+def hello_world():
+ return 'Hello, World!'
