68bd7456e43b88522b015db816b5d71e1c50d530
PROMETHEUS <prometheus@agent.local>
2026-06-25 20:30:03 +0000

    PROMETHEUS: Auto-Sync 2026-06-25 20:30:03 UTC

---
+++ set_display.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export DISPLAY=:0
+echo 'Display set to :0' >> /root/local_agent/agent_workspace/log.txt
---
+++ test2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 'Hello from test2.sh' && ./test2.sh
+# Hinzufügen eines Testbefehls: echo 'Test script executed successfully'
