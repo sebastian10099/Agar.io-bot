003e085fe810792c14ac746908860c1a5a611f3e
PROMETHEUS <prometheus@agent.local>
2026-06-26 21:20:03 +0000

    PROMETHEUS: Auto-Sync 2026-06-26 21:20:03 UTC

--- check_port_5001.sh
+++ check_port_5001.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-netstat -tuln | grep :5001
+# Check if port 5001 is open
+if lsof -i :5001 | grep -q LISTEN; then
+echo "Port 5001 ist offen."
+else
+echo "Port 5001 ist nicht offen."
+fi
