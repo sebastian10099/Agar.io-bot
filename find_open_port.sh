cc4350fe668070cfebd33785902f1b9e26addc0e
PROMETHEUS <prometheus@agent.local>
2026-06-26 13:20:03 +0000

    PROMETHEUS: Auto-Sync 2026-06-26 13:20:03 UTC

--- find_open_port.sh
+++ find_open_port.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# Start the port scan process
-curl -s https://raw.githubusercontent.com/hexagon23/portscanner/master/portscanner.py | python3 --port-scan 1234567890
+# Find a free port and save it in PORT variable
+PORT=$(netstat -tuln | grep :80/tcp | awk '{print $4}' | cut -d: -f2)
+if [ -z "$PORT" ]; then
+    PORT=81
+else
+    if ! (netcat -zv localhost $PORT 5 &>/dev/null); then
+        PORT=$PORT
+    else
+        PORT=$(shuf -i 49153-65535 -n 1)
+    fi
+echo "PORT=$PORT" >> /root/local_agent/agent_workspace/port_vars.txt
+fi
