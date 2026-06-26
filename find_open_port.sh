8c0e21c858b430624d13d2e59804fc1b31ee9045
PROMETHEUS <prometheus@agent.local>
2026-06-26 13:30:03 +0000

    PROMETHEUS: Auto-Sync 2026-06-26 13:30:03 UTC

--- find_open_port.sh
+++ find_open_port.sh
@@ -1,13 +1,4 @@
+
 #!/bin/bash
-# Find a free port and save it in PORT variable
-PORT=$(netstat -tuln | grep :80/tcp | awk '{print $4}' | cut -d: -f2)
-if [ -z "$PORT" ]; then
-    PORT=81
-else
-    if ! (netcat -zv localhost $PORT 5 &>/dev/null); then
-        PORT=$PORT
-    else
-        PORT=$(shuf -i 49153-65535 -n 1)
-    fi
-echo "PORT=$PORT" >> /root/local_agent/agent_workspace/port_vars.txt
-fi
+PORT=$(netstat -tuln | grep ':80 ' | cut -d: -f4 | awk \'{print $1}\' | sort -u)
+echo "OPEN_PORT=$PORT"
