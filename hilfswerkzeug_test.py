4db1dc20f907fda72a6347b045c6459e9d0c3e0e
PROMETHEUS <prometheus@agent.local>
2026-06-27 01:50:03 +0000

    PROMETHEUS: Auto-Sync 2026-06-27 01:50:03 UTC

--- hilfswerkzeug_test.py
+++ hilfswerkzeug_test.py
@@ -1,2 +1,7 @@
+#!/usr/bin/env python3
 import hilfswerkzeug
-hilfswerkzeug.hilfswerkzeug()
+
+def test_update_documentation():
+    assert 'Hilfswerkzeug verfügbar.' in hilfswerkzeug.update_documentation()
+if __name__ == '__main__':
+    test_update_documentation()
