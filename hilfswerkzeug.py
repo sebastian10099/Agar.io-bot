1fd5bc4cad126f79ab1be112eb2a488fb03fa2ea
PROMETHEUS <prometheus@agent.local>
2026-06-27 00:50:03 +0000

    PROMETHEUS: Auto-Sync 2026-06-27 00:50:03 UTC

--- hilfswerkzeug.py
+++ hilfswerkzeug.py
@@ -1,8 +1,1 @@
-import flask
-from flask import Flask
-app = Flask(__name__)
-@app.route('/status', methods=['GET'])
-def status():
-    return 'Status OK'
-if __name__ == '__main__':
-    app.run(debug=True)
+print('Willkommen bei unserem Hilfswerkzeug!')
