06e8375ef115ddc3c2df614cdefa7728891bf125
PROMETHEUS <prometheus@agent.local>
2026-06-27 06:00:03 +0000

    PROMETHEUS: Auto-Sync 2026-06-27 06:00:03 UTC

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python3
-from flask import Flask, render_template
+from flask import Flask, jsonify
+import json
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+
+@app.route('/values')
+def get_values():
+    with open('/root/local_agent/agent_workspace/values.json', 'r') as f:
+        values = json.load(f)
+    return jsonify(values)
+
+if __name__ == '__main__':
+    app.run(debug=True)
